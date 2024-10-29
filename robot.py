c42f6a281f3a15cb16673b794f8791f8e518e098
yagoszn <162775079+yagoszn@users.noreply.github.com>
2024-10-29 10:32:58 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 from gpiozero import RGBLED
 from colorzero import Color
+
 motor_L = Motor (forward=17, backward=27)
 motor_R = Motor (forward=22, backward=23)
 sensor = DistanceSensor (echo=5, trigger=20)
@@ -18,29 +19,29 @@ rightStr = "Reversing Right \n -_^"
 leftStr = "Reversing Left \n ^_-"
 
 def moveForward():
-motor_L.forward(speed = 0.4)
-motor_R.forward(speed = 0.4)
-print("Moving Forward")
+  motor_L.forward(speed = 0.4)
+  motor_R.forward(speed = 0.4)
+  print("Moving Forward")
 
 def moveBackward():
-motor_L.backward(speed = 0.3)
-motor_R.backward(speed = 0.3)
-print("Moving Backward")
+  motor_L.backward(speed = 0.3)
+  motor_R.backward(speed = 0.3)
+  print("Moving Backward")
 
 def robotStop():
-motor_L.forward(speed = 0)
-motor_R.backward(speed = 0)
-print("Robot Stop")
+  motor_L.forward(speed = 0)
+  motor_R.backward(speed = 0)
+  print("Robot Stop")
 
 def moveBackwardRight():
-motor_L.backward(speed = 0.3)
-motor_R.backward(speed = 0.6)
-print("Moving Backward Right")
+  motor_L.backward(speed = 0.3)
+  motor_R.backward(speed = 0.6)
+  print("Moving Backward Right")
 
 def moveBackwardLeft():
-motor_L.backward(speed = 0.6)
-motor_R.backward(speed = 0.3)
-print ("Moving Backward Left")
+  motor_L.backward(speed = 0.6)
+  motor_R.backward(speed = 0.3)
+  print ("Moving Backward Left")
 
 i2c = board.I2C()
 disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
@@ -52,29 +53,33 @@ width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
+
 def display_message(top_line, line_2):
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-draw.text((0, 0), top_line, font=large_font, fill=255)
-draw.text((0, 50), line_2, font=small_font, fill=255)
-disp.image(image)
-disp.show()
+  draw.rectangle((0,0,width,height), outline=0, fill=0)
+  draw.text((0, 0), top_line, font=large_font, fill=255)
+  draw.text((0, 50), line_2, font=small_font, fill=255)
+  disp.image(image)
+  disp.show()
 
 rgb_led = RGBLED(12, 6, 13)
 red = 0
 green = 0
 blue = 0
+
 def toGreen():
-rgb_led.off()
-rgb_led.on()
-rgb_led.color = Color(0, 255, 0)
+  rgb_led.off()
+  rgb_led.on()
+  rgb_led.color = Color(0, 255, 0)
+
 def toRed():
-rgb_led.off()
-rgb_led.on()
-rgb_led.color = Color(255, 0, 0)
+  rgb_led.off()
+  rgb_led.on()
+  rgb_led.color = Color(255, 0, 0)
+
 def toYellow():
-rgb_led.off()
-rgb_led.on()
-rgb_led.color = Color(255, 48, 0)
+  rgb_led.off()
+  rgb_led.on()
+  rgb_led.color = Color(255, 48, 0)
 
 count=0
 
